a7b066b54353ede5a8f4ff5a134b0db5517494d6
Erin Pintozzi <epintozzi@turing.edu>
2021-11-30 10:05:39 -0800

    class demo of webmock and vcr

---
+++ spec/features/search_congress_members_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Govt Search' do
+  describe 'happy path' do
+    it 'allows user to search for govt members', :vcr do
+      # json_response = File.read('spec/fixtures/members_of_the_senate.json')
+      # stub_request(:get, "https://api.propublica.org/congress/v1/116/senate/members.json").
+      # with(
+      #   headers: {
+      #  'X-Api-Key'=> ENV['govt_api_key']
+      #   }).
+      # to_return(status: 200, body: json_response, headers: {})
+
+      visit root_path
+
+      fill_in :search, with: "Sanders"
+      click_button "Search"
+
+      expect(page.status_code).to eq 200
+      expect(page).to have_content('Senator Bernard Sanders was found!')
+      expect(page).to have_content('SenSanders')
+    end
+
+    it 'allows user to search for another govt member' do
+      VCR.use_cassette('propublica_members_of_the_senate', re_record_interval: 7.days) do
+        visit root_path
+
+        fill_in :search, with: 'Booker'
+        click_button 'Search'
+
+        expect(page.status_code).to eq 200
+        expect(page).to have_content("Senator Cory Booker was found!")
+        expect(page).to have_content("SenBooker")
+      end
+    end
+  end
+end
