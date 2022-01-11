ba689d9af26bfc535a1d51d26911c9b46e9c1adb
Erin Pintozzi <epintozzi@turing.edu>
2022-01-10 21:26:50 -0600

    setup repo for sidekiq lesson

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,8 @@
+class UserNotifierMailer < ApplicationMailer
+  def send_randomness_email(email, thought)
+    @email = email
+    sleep 5 # this is for demonstration purposes of feeling the pain of a long page load.
+    mail( :to => @email,
+    :subject => 'Here is your randomness email of awesome.' )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  get 'mailers/create'
+  get 'home/show'
+  root to: "home#show"
+
+  resources :mailers, only: [:create]
+
+  get "/sent", to: "mailers#sent"
 end
