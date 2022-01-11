8deb3b4f61e23afd6ab262f2b03e588c4acd8e2b
Meg Stang <mstang15@gmail.com>
2022-01-11 08:45:38 -0700

    Add two new views

--- app/controllers/congress_controller.rb
+++ app/controllers/congress_controller.rb
@@ -8,13 +8,10 @@ class CongressController < ApplicationController
 
   def search
     @member = CongressFacade.find_senate_member(params[:search])
-    #rendering view 
-    render "welcome/index"
   end
 
   def search_state
     state = params[:state]
     @house_members = CongressFacade.house_members_by_state(state)
-    render "welcome/index"
   end
 end
